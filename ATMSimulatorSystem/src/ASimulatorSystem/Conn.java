551fc6d792427378305eaf7ec66352e9b5423ce2
Jaya Banduke <jayabanduke@gmail.com>
2021-08-12 18:57:12 +0530

    First Coomit

---
+++ ATMSimulatorSystem/src/ASimulatorSystem/Conn.java
@@ -0,0 +1,22 @@
+package ASimulatorSystem;
+import java.sql.*; 
+
+public class Conn {
+	Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///ams","root","");   
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+
+	
+
+}
